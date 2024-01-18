7d2512120b0635ebd43a121630618c14d5a9c312
Christian Nielsen <chrnielsen2003@gmail.com>
2024-01-18 03:03:59 +0100

    Add cors, date-fns, and nodemailer dependencies

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 const express = require("express");
 const app = express();
 const port = 6969;
+const { format, fromUnixTime } = require("date-fns");
+var nodemailer = require("nodemailer");
+const cors = require("cors");
+app.use(cors());
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "chrnielsen2003@gmail.com",
+    pass: "dcql iave mxcf vhmv",
+  },
+});
+
+function sendMail(action, time, data) {
+  var mailOptions = {
+    from: "chrnielsen2003@gmail.com",
+    to: "loui.papi@gmail.com",
+    subject: `${action} ${time} ${data}`,
+    text: `${action} tid: ${time} ${data}`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
 
 app.get("/api/start", (req, res) => {
-  console.log("start");
+  let timestamp = new Date().getTime();
+  let unixTimestamp = timestamp;
+  let date = fromUnixTime(unixTimestamp / 1000);
+  const data = req.query;
+  let formattedDate = format(date, "dd-MM-yyyy");
+  let formattedTime = format(date, "HH:mm");
 
-  res.send("Hello, world!");
+  console.log("klokken er:", formattedTime);
+
+  sendMail(
+    `${data.firstName} har stemplet ${data.checkId}`,
+    formattedTime,
+    formattedDate
+  );
+
+  console.log(data);
+  res.json({ time: formattedTime, date: formattedDate });
 });
 
 app.listen(port, () => {
